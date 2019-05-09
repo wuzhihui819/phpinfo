bbd70470df4d23c7450246f92d68e62026b620fe
wuzhihui819 <wuzhihui819@163.com>
2019-05-09 15:26:21 +0800

    Init Commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use Encore\Admin\Layout\Content;
+use Encore\PHPInfo\PHPInfo;
+
+$path = PHPInfo::config('path', 'phpinfo');
+
+Route::get($path, function (Content $content, PHPInfo $info) {
+    $info = $info->toCollection();
+
+    return $content
+        ->header('PHP\'s configuration')
+        ->description(' ')
+        ->body(view('phpinfo::phpinfo', compact('info')));
+});
